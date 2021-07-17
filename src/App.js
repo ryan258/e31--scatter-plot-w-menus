bb6dfa160fae8e48374711c8e1a8f3347681aa0c
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-07-17 12:32:51 -0500

    Change axis labels on changes to x and y

--- src/App.js
+++ src/App.js
@@ -22,6 +22,22 @@ const margin = {
 const xAxisLabelOffset = 50
 const yAxisLabelOffset = 40
 
+const attributes = [
+  { value: 'sepal_length', label: 'Sepal Length' },
+  { value: 'sepal_width', label: 'Sepal Width' },
+  { value: 'petal_length', label: 'Petal Length' },
+  { value: 'petal_width', label: 'Petal Width' },
+  { value: 'species', label: 'Species' }
+]
+
+const getLabel = (value) => {
+  for (let i = 0; i < attributes.length; i++) {
+    if (attributes[i].value === value) {
+      return attributes[i].label
+    }
+  }
+}
+
 const App = () => {
   const data = useData()
 
@@ -29,13 +45,13 @@ const App = () => {
   const [xAttribute, setXAttribute] = useState(initialXAttribute)
   // console.log(xAttribute)
   const xValue = (d) => d[xAttribute]
-  const xAxisLabel = 'Petal Length'
+  const xAxisLabel = getLabel(xAttribute)
 
   const initialYAttribute = 'sepal_width'
   const [yAttribute, setYAttribute] = useState(initialYAttribute)
   // console.log(yAttribute)
   const yValue = (d) => d[yAttribute]
-  const yAxisLabel = 'Sepal Width'
+  const yAxisLabel = getLabel(yAttribute)
 
   if (!data) {
     return <pre>'Loading...'</pre>
@@ -46,14 +62,6 @@ const App = () => {
 
   const innerHeight = height - margin.top - margin.bottom
   const innerWidth = width - margin.right - margin.left
-
-  const attributes = [
-    { value: 'sepal_length', label: 'Sepal Length' },
-    { value: 'sepal_width', label: 'Sepal Width' },
-    { value: 'petal_length', label: 'Petal Length' },
-    { value: 'petal_width', label: 'Petal Width' },
-    { value: 'species', label: 'Species' }
-  ]
 
   const xScale = scaleLinear() //
     .domain(extent(data, xValue))
